063d8741be072855647683d6adc2454a23917e1b
Dave <dave.w.mckinnon@gmail.com>
2022-04-19 19:42:53 -0700

    remove errors caused by function in wrong file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
-import DayListItem from "components/DayListItem"
 
 ReactDOM.render(<Application />, document.getElementById("root"));
