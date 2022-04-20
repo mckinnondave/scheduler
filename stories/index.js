daad6646d80c48d61f42b61a9803ff9977f17f17
Dave <dave.w.mckinnon@gmail.com>
2022-04-20 14:44:10 -0700

    add story for Confirm

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -156,3 +157,10 @@ storiesOf("Appointment", module)
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
   />))
+  .add("Confirm", () => (
+    <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />
+  ))
