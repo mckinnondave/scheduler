b971b73526f085d57405709ae38be077a2e6cdeb
Dave <dave.w.mckinnon@gmail.com>
2022-04-19 19:36:27 -0700

    create InterviewerListItem and refactor

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const interviewerListItemClass = classNames("interviewers__item", {
+  "interviewers__item--selected": selected
+  });
+
+  return (
+    <li 
+    className={interviewerListItemClass}
+    onClick={() => setInterviewer(id)}
+    >
+  <img
+    className="interviewers__item-image"
+    src={avatar}
+    alt={name}
+  />
+  {selected && name}
+</li>
+  )
+}
