619e3039bcfbceb17ffc6adef0dde31e1f4ba898
Dave <dave.w.mckinnon@gmail.com>
2022-04-20 17:43:20 -0700

    add header and show ternary operator for show/empty

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header"
+import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty"
 
 export default function Appointment(props) {
+  const { /*id*/ time, interview } = props;  
   return (
-    <article className="appointment">{props.time ? `Appointment at ${props.time}` : "No Appointment"}</article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+    </article>
   )
 }
