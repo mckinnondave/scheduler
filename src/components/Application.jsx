229af63f0270e7b807cad82cd10d116685e1f0fd
Dave <dave.w.mckinnon@gmail.com>
2022-04-21 18:28:11 -0700

    add promises for days, appointments, interviewers, and set states

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,63 +2,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"
 import "components/Application.scss";
 import DayList from "components/DayList";
-import Appointment from "components/Appointment/index"
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
+import Appointment from "components/Appointment/index";
+import { getAppointmentsForDay } from "helpers/selectors";
+import { getInterview } from "helpers/selectors";
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-  const appointmentsArr = Object.values(appointments)
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+  const setDay = day => setState({...state, day})
+  const appointments = getAppointmentsForDay(state, state.day)
 
   useEffect(() => {
-    axios.get(`/api/days`)
-      .then ((res) => {
-        setDays([...res.data]);
-        console.log("RESPONSE", res.data);
-      })
-      .catch((error) => {
-        console.log(`AXIOS ERROR:`, error);
-      })
-  }, [day])
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      console.log(all[0]);
+      console.log(all[1]);
+      console.log(all[2]);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    });
+  }, []) // added brackets to stop logging. Might need to change
 
   return (
     <main className="layout">
@@ -71,8 +39,8 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
         <nav className="sidebar__menu"></nav>
@@ -85,11 +53,14 @@ export default function Application(props) {
 
       {/* Maps over appointments array and returns individual appointments */}
       <section className="schedule">
-        {appointmentsArr.map(appointment => {
+        {appointments.map(appointment => {
+          const interview = getInterview(state, appointment.interview)
           return (
             <Appointment 
             key={appointment.id}
-            {...appointment}
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview}
             />
           )
         })}
