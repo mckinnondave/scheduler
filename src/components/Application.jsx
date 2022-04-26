d93971b02ad5e635c690e41ffd7a34cdbea02209
Dave <dave.w.mckinnon@gmail.com>
2022-04-25 18:44:06 -0700

    refactor and move state related code to useApplicationData

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,32 +1,36 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios"
+import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationdata";
+
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  })
-  const setDay = day => setState({...state, day})
-  const appointments = getAppointmentsForDay(state, state.day)
+
+  const {
+    state, 
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData()
+  
   const interviews = getInterviewersForDay(state, state.day)
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then((all) => {
-      console.log(all[0]);
-      console.log(all[1]);
-      console.log(all[2]);
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    });
-  }, []) // added brackets to stop logging. Might need to change
+  const dayAppointments = getAppointmentsForDay(state, state.day).map(appointment => {
+    const interview = getInterview(state, appointment.interview)
+    return (
+      <Appointment 
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+      interviewers={interviews}
+      bookInterview={bookInterview}
+      deleteInterview={deleteInterview}
+      />
+    )
+  })
 
   return (
     <main className="layout">
@@ -51,20 +55,8 @@ export default function Application(props) {
         />
       </section>
 
-      {/* Maps over appointments array and returns individual appointments */}
       <section className="schedule">
-        {appointments.map(appointment => {
-          const interview = getInterview(state, appointment.interview)
-          return (
-            <Appointment 
-            key={appointment.id}
-            id={appointment.id}
-            time={appointment.time}
-            interview={interview}
-            interviewers={interviews}
-            />
-          )
-        })}
+        {dayAppointments}
         <Appointment key="last" time="5pm" />
       </section>
 
