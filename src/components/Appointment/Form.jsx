648f62b45b4be5f125a9da63da5be5848559e1ac
Dave <dave.w.mckinnon@gmail.com>
2022-04-26 14:24:25 -0700

    add valiate function to Form

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -3,9 +3,9 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
-  console.log("PROPS", props);
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("")
 
   function reset() {
     setStudent("");
@@ -16,6 +16,18 @@ export default function Form(props) {
     reset();
     props.onCancel()
   };
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer")
+      return;
+    }
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -28,8 +40,10 @@ export default function Form(props) {
             value={student}
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           onChange={setInterviewer}
@@ -41,7 +55,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
