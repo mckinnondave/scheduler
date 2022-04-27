c5f82fc3b2ba1c5a3d2acc745cabefa6cd42c353
Dave <dave.w.mckinnon@gmail.com>
2022-04-27 09:17:12 -0700

    remove example tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -10,13 +10,4 @@ describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
-
-  // The following two test examples are for illustrative purposes only 
-  it("does something it is supposed to do", () => {
-    // ...
-  });
-
-  it("does something else it is supposed to do", () => {
-    // ...
-  });
 });
